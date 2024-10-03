494bbc9152ed31ccf55bdcca7c34f1b2413977aa
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-10-03 13:58:02 -0400

    initial

---
+++ cypress.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:3000',
+    video: true,
+    supportFile: false,
+    fixturesFolder: './fixtures',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+})
